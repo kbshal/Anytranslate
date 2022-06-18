0226485d149cee9178ac587acfd3e524425d0a92
ashimdahal <bboxchmpx@gmail.com>
2022-06-18 18:35:03 +0545

    defined r from previous commit with proxy

--- api/PYtranslator.py
+++ api/PYtranslator.py
@@ -9,8 +9,11 @@ from urllib.parse import quote
 import urllib3
 import logging
 import asyncio
-from api.constant import LANGUAGES, DEFAULT_SERVICE_URLS
-
+try:
+    from api.constant import LANGUAGES, DEFAULT_SERVICE_URLS
+except:
+    from constant import LANGUAGES, DEFAULT_SERVICE_URLS
+    
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
@@ -106,6 +109,13 @@ class google_translator:
                                     headers=headers,
                                     )
         try:
+            if self.proxies == None or type(self.proxies) != dict:
+                self.proxies = {}
+            with requests.Session() as s:
+                s.proxies = self.proxies
+                r = s.send(request=response.prepare(),
+                           verify=False,
+                           timeout=self.timeout)
             for line in r.iter_lines(chunk_size=1024):
                 decoded_line = line.decode('utf-8')
                 if "MkEWBc" in decoded_line:
@@ -176,6 +186,13 @@ class google_translator:
                                     data=freq,
                                     headers=headers)
         try:
+            if self.proxies == None or type(self.proxies) != dict:
+                self.proxies = {}
+            with requests.Session() as s:
+                s.proxies = self.proxies
+                r = s.send(request=response.prepare(),
+                           verify=False,
+                           timeout=self.timeout)
             for line in r.iter_lines(chunk_size=1024):
                 decoded_line = line.decode('utf-8')
                 if "MkEWBc" in decoded_line:
